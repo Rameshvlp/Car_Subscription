58bff7f7cb1eaa596951c446923ed72ae42bab1d
Rameshvlp <rameshvlp2001@gmail.com>
2024-01-27 12:34:04 +0530

    card

--- routes/carroute.js
+++ routes/carroute.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const route =  express.Router();   
 const path = require('path');
-const {insertc, retriveC} = require("../sevices/carService") 
+const {insertc, retriveC} = require("../sevices/carService"); 
+const { chownSync } = require("fs");
 
 route.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, '../FrontEnd', 'Retrive.html'))
